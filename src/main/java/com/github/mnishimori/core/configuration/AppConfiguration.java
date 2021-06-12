4e8c90277aaab3ea88ff9d8662b51ce7bd0795a9
makn <mnishimori@yahoo.com.br>
2021-06-12 12:35:51 -0300

    Refactoring configurations and user classes

--- src/main/java/com/github/mnishimori/core/configuration/AppConfiguration.java
+++ src/main/java/com/github/mnishimori/core/configuration/AppConfiguration.java
@@ -1,18 +1,27 @@
 package com.github.mnishimori.core.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@PropertySource(
+		value = {"file://${HOME}/.spring-boot-app.properties",
+				"file:C:/Program Files/Java/.spring-boot-app.properties"}, ignoreResourceNotFound = true)
 public class AppConfiguration {
+	
+	@Autowired
+	private Environment env;	
 	
 	@Bean(name = "expirationAppTime")
 	public Long expirationAppTime() {
-		return 30L;
+		return Long.parseLong(env.getProperty("expirationAppTime"));
 	}
 	
 	@Bean(name = "signatureKey")
 	public String signatureKey() {
-		return "ZGV2cyBnY3RpIGFnaXI=";
+		return env.getProperty("signatureKey");
 	}
 }
